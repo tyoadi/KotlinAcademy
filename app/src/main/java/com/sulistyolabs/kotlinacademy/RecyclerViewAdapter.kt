2fb2af1f2c763ba28d4fcbfb2f63c034b88dfc5a
tyoadi <sulistyo.adi32@gmail.com>
2018-10-18 04:30:56 +0700

    update

--- app/src/main/java/com/sulistyolabs/kotlinacademy/RecyclerViewAdapter.kt
+++ app/src/main/java/com/sulistyolabs/kotlinacademy/RecyclerViewAdapter.kt
@@ -3,25 +3,34 @@ package com.sulistyolabs.kotlinacademy
 import android.content.Context
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import com.squareup.picasso.Picasso
+import java.text.FieldPosition
 
-class RecyclerViewAdapter(private val contex: Context, private val item: List<Item>)
+class RecyclerViewAdapter(private val contex: Context, private val items: List<Item>)
     : RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> (){
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
-            RecyclerView.ViewHolder(LayoutInflater.from(contex).inflate(R.layout.item_list, parent, false)) {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun onCreateViewHolder(parent: ViewGroup, viewGroup: Int) =
+            ViewHolder(LayoutInflater.from(contex).inflate(R.layout.item_list, parent , false))
 
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bindItem(items[position])
+    }
 
-                override fun getItemCount(): Int {
-                    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-                }
+    override fun getItemCount(): Int = items.size
 
-                override fun onBindViewHolder(p0: RecyclerViewAdapter.ViewHolder, p1: Int) {
-                    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-                }
+    class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
 
-                class ViewHolder {
+        private val name = view.findViewById<TextView>(R.id.name)
+        private val image = view.findViewById<ImageView>(R.id.image)
 
-                }
-            }
+        fun bindItem(items: Item) {
+            name.text = items.name
+            items.image?.let { Picasso.get().load(it).into(image) }
+        }
+
+    }
+}
